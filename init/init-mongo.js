0df49408e27572230109f4f6227c591ded6efdf1
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-09-09 18:16:17 +0200

    .

--- init/init-mongo.js
+++ init/init-mongo.js
@@ -10,15 +10,64 @@ db.createUser({
 });
 db.createCollection("users");
 db.users.insertOne({
-  name: "Bill Palmer",
-  email: "bpalmer@gmail.com"
+  id: 1,
+  name: "Max Svensson",
+  email: "me@gmail.com"
 });
 db.users.insertOne({
-  name: "Lilly Smith",
-  email: "lsmith@gmail.com"
+  id: 2,
+  name: "Jarl Svensson",
+  email: "js@gmail.com"
 });
+db.users.insertOne({
+  id: 3,
+  name: "Harisha Svensson",
+  email: "hs@gmail.com",
+});
+db.users.insertOne({
+  id: 4,
+  name: "Dennis Svensson",
+  email: "ds@gmail.com",
+});
+db.users.insertOne({
+  id: 5,
+  name: "Simon Svensson",
+  email: "ss@gmail.com",
+});
+db.users.insertOne({
+  id: 6,
+  name: "Zoreh Svensson",
+  email: "zs@gmail.com",
+});
+
 db.createCollection("orders");
 db.orders.insertOne({
-  order: "Pizza",
-  price: 10.99,
+  order: "Samsung Galaxy S10",
+  price: 999.99,
+  user_id: 2,
+});
+db.orders.insertOne({
+  order: "LG G8 ThinQ",
+  price: 1499.99,
+  user_id: 6,
+});
+db.orders.insertOne({
+  order: "MSI GS65 Stealth Thin",
+  price: 499.99,
+  user_id: 5,
+});
+db.orders.insertOne({
+  order: "Bowers & Wilkins PX7",
+  price: 799.99,
+  user_id: 4,
+});
+db.orders.insertOne({
+  order: "Apple AirPods Pro",
+  price: 699.99,
+  user_id: 3,
+});
+db.orders.insertOne({
+  order: "Air Jordan 1 Retro High OG",
+  price: 2099.99,
+  user_id: 1,
 });
