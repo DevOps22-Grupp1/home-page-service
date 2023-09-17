f5134c7c86e93ac623e51a4d20805085ca3fa54d
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-09-17 17:46:18 +0200

    added error handling

--- backend/app.py
+++ backend/app.py
@@ -24,50 +24,51 @@ def about():
 
 @app.route("/products/")
 def products():
-    response = requests.get('http://scamazon-product-catalog-service-1:4005/api/products') 
 
-    if response.status_code == 200:
-        products = json.loads(response.text)
-    else:
-        return jsonify({'error': 'Failed to fetch data'})
+    try:
+        response = requests.get('http://scamazon-product-catalog-service-1:4005/api/products') 
+        if response.status_code == 200:
+            products = json.loads(response.text)
+    except requests.exceptions.RequestException as e:
+        products = 'Failed to fetch data'
   
     return render_template("products.html", products=products)
-#  images=images, prices=prices
 
-    # look in products.html how to use the forloop(array) correct and not making it go double. At this moment it goes double for visual purpose #
-    # status is just a demo list how it could look like on the page #
+
+    ### Make requests to the other services and return the result ok or not ok ###
 @app.route("/server/")
 def server():
+    try:
+        res1 = requests.get("http://scamazon-product-catalog-service-1:4005")
+        if res1.status_code == 200:
+            r1 = res1.text
+    except requests.exceptions.RequestException as e:
+        r1 = 'Failed to fetch data'
+    
+    
+    try:
+        res2 = requests.get("http://scamazon-user-management-service-1:4006")
+        if res2.status_code == 200:
+            r2 = res2.text
+    except requests.exceptions.RequestException as e:
+        r2 = 'Failed to fetch data'
+    
+    
+    try:
+        res3 = requests.get("http://scamazon-order-processing-service-1:4007")
+        if res3.status_code == 200:
+            r3 = res3.text
+    except requests.exceptions.RequestException as e:
+        r3 = 'Failed to fetch data'
+    
 
-    res1 = requests.get("http://scamazon-product-catalog-service-1:4005")
-    if res1.status_code == 200:
-        r1 = "Success"
-    else:
-        r1 = jsonify({'error': 'Failed to fetch data'})
     
-    res2 = requests.get("http://scamazon-user-management-service-1:4006")
-    if res2.status_code == 200:
-        r2 = "Success"
-    else:
-        r2 = jsonify({'error': 'Failed to fetch data'})
-    
-    res3 = requests.get("http://scamazon-order-processing-service-1:4007")
-    if res3.status_code == 200:
-        r3 = "Success"
-    else:
-        r3 = jsonify({'error': 'Failed to fetch data'})
-
-    
-    status = [{"name": "product-catalog-service", "status": r1.text}, {"name": "user-managament-service", "status": r2.text}, {"name": "order-processing-service", "status": r3.text}]
+    status = [{"name": "product-catalog-service", "status": r1}, {"name": "user-managament-service", "status": r2}, {"name": "order-processing-service", "status": r3}]
 
 
     return render_template("server.html", utc_dt=datetime.datetime.utcnow(), status=status)
-### Make requests to the other services and return the result ok or not ok ###
 
-#def check_server_status():
-    # r1 = requests.get("http://order-processing-service:5001")
-    # r2 = requests.get("http://scamazon-product-catalog-service-1:4005/")
-    # return f"order-processing-service says: {r1.text}! - user-managament-service says {r2.text} - product-catalog-service says {r3.text}" 
+ 
 
 
 # Very important to disable debug mode
