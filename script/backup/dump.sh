f3e049419d5ac862fdbaaa7a1353cf6dd89c326c
webcrunch <jarllinduist@gmail.com>
2023-10-10 14:24:59 +0200

    remove file name for only date

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -5,7 +5,7 @@ echo Starting-BACKUP
 echo ******************************************************
 NOW="$(date +"%F")-$(date +"%T")"
 
-FILE="$DB_NAME-$NOW"
+FILE="$DB_NAME"
 
 # mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
 mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
--- script/backup/restore.sh
+++ script/backup/restore.sh
@@ -5,7 +5,7 @@ echo Starting-BACKUP
 echo ******************************************************
 NOW="$(date +"%F")-$(date +"%T")"
 
-FILE="$DB_NAME-$NOW"
+FILE="$DB_NAME"
 
 # mongorestore --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
 mongorestore --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --verbose /mongodump/db/$FILE
