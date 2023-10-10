b1eaf3a93c49b6284d9f2327ebab5a2e92c5c7a1
webcrunch <jarllinduist@gmail.com>
2023-10-10 15:23:50 +0200

    test again

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -4,8 +4,9 @@ echo ******************************************************
 echo Starting-BACKUP
 echo ******************************************************
 # NOW="$(date +"%F")-$(date +"%T")"
-FILE="$DB_NAME"
+NOW=$(date + %H)
+FILE="$DB_NAME-$NOW"
 
 # mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
 mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
-sleep 30 | echo End-BACKUP
+sleep 30 | echo End-BACKUP FOR $FILE
