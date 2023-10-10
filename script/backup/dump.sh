316451bea27a9408708f30839e7e29fca8f9e627
webcrunch <jarllinduist@gmail.com>
2023-10-10 15:06:52 +0200

    add file for just name

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -4,10 +4,8 @@ echo ******************************************************
 echo Starting-BACKUP
 echo ******************************************************
 # NOW="$(date +"%F")-$(date +"%T")"
-NOW=$(date + %H)
-echo $DB_NAME-$now $DB_NAME
-FILE="$DB_NAME-$now"
+FILE="$DB_NAME"
 
 # mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
 mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
-sleep 30 | echo End-BACKUP FOR $FILE
+sleep 30 | echo End-BACKUP
