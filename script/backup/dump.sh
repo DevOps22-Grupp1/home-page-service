af62cd8ac8ffb645e9f32168b7249c43f4addda3
webcrunch <jarllinduist@gmail.com>
2023-10-10 12:33:00 +0200

    command instead of print

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -8,5 +8,5 @@ NOW="$(date +"%F")-$(date +"%T")"
 FILE="$DB_NAME-$NOW"
 
 # mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
-echo working ? $MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT
+mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
 sleep 30 | echo End-BACKUP
