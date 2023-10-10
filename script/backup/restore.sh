7aa298a72fca818b34587a57db8f77c4b15f89a6
webcrunch <jarllinduist@gmail.com>
2023-10-10 14:04:02 +0200

    handle restore script

---
+++ script/backup/restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ******************************************************
+echo Starting-BACKUP
+echo ******************************************************
+NOW="$(date +"%F")-$(date +"%T")"
+
+FILE="$DB_NAME-$NOW"
+
+mongorestore --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
+
+sleep 30 | echo End-BACKUP
