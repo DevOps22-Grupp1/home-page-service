f1cf89cb78bbb9b3ad98fb11e602478a3c8c00cb
webcrunch <jarllinduist@gmail.com>
2023-10-10 14:22:15 +0200

    uri on mongorestore

--- script/backup/restore.sh
+++ script/backup/restore.sh
@@ -8,6 +8,6 @@ NOW="$(date +"%F")-$(date +"%T")"
 FILE="$DB_NAME-$NOW"
 
 # mongorestore --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
-mongorestore --verbose /mongodump/db/$FILE
+mongorestore --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --verbose /mongodump/db/$FILE
 
 sleep 30 | echo End-BACKUP
