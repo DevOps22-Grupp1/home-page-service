827567ff5b09ae25779f164bcec9a6b765bfbfaa
webcrunch <jarllinduist@gmail.com>
2023-10-11 10:02:03 +0200

    will this work ?

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -7,7 +7,7 @@ echo ******************************************************
 # NOW=$(date + %H)
 
 EXTEND=cd /mongodump/db/ ls -1 | wc -l
-FILE="$DB_NAME_$EXTEND"
+FILE=$DB_NAME_$EXTEND
 # mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
 mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
 sleep 30 | echo End-BACKUP FOR $FILE
