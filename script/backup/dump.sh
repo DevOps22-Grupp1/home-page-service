8d22ec31b883c9947dcfbecaef822a2f71dce2af
webcrunch <jarllinduist@gmail.com>
2023-10-11 09:43:02 +0200

    test: count the dump files

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -7,7 +7,7 @@ echo ******************************************************
 NOW=$(date + %H)
 echo $(date + %H)
 FILE="$DB_NAME"
-
+echo cd /mongodump/db/ ls -1 | wc -l
 # mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
 mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
 sleep 30 | echo End-BACKUP FOR $FILE
