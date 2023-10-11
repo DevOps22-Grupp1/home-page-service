1172f969ec2cc11d5dd756203d93bcceb96cfa40
webcrunch <jarllinduist@gmail.com>
2023-10-11 09:51:53 +0200

    changing file name

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -4,10 +4,10 @@ echo ******************************************************
 echo Starting-BACKUP
 echo ******************************************************
 # NOW="$(date +"%F")-$(date +"%T")"
-NOW=$(date + %H)
-echo $(date + %H)
-FILE="$DB_NAME"
-echo cd /mongodump/db/ ls -1 | wc -l
+# NOW=$(date + %H)
+
+EXTEND=cd /mongodump/db/ ls -1 | wc -l
+FILE="$DB_NAME_$EXTEND"
 # mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
 mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
 sleep 30 | echo End-BACKUP FOR $FILE
