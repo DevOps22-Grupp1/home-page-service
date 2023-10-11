f57b77874309e30eee679b7d1505619274ed0768
webcrunch <jarllinduist@gmail.com>
2023-10-11 10:12:37 +0200

    test again :)

--- script/backup/dump.sh
+++ script/backup/dump.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo ******************************************************
-echo Starting-BACKUP
-echo ******************************************************
-# NOW="$(date +"%F")-$(date +"%T")"
-# NOW=$(date + %H)
+echo "****************************************************"
+echo "Starting-BACKUP"
+echo "****************************************************"
 
-EXTEND=cd /mongodump/db/ ls -1 | wc -l
-FILE=$DB_NAME_$EXTEND
-# mongodump --uri=mongodb://$MONGODB_URI:$MONGOD_USER@$MONGOD_HOST:$MONGOD_PORT/$DB_NAME  --out=/mongodump/db/$FILE
-mongodump --uri=$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT --out=/mongodump/db/$FILE
-sleep 30 | echo End-BACKUP FOR $FILE
+EXTEND=$(ls -1 /mongodump/db/ | wc -l)
+FILE="${DB_NAME}_${EXTEND}"
+
+mongodump --uri="$MONGODB_URI$DB_USERNAME:$DB_PASSWORD@$MONGOD_HOST:$MONGOD_PORT" --out="/mongodump/db/$FILE"
+sleep 30
+echo "End-BACKUP FOR $FILE"
