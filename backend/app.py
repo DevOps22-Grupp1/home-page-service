9eb96b42437cb1d5991bc8a0a14d675219992aa2
DennisNevback <74974974+DennisNevback@users.noreply.github.com>
2024-02-03 20:50:54 +0100

    Fixed stripe

--- backend/app.py
+++ backend/app.py
@@ -188,8 +188,7 @@ def server():
 @app.route('/create-checkout-session', methods=['POST'])
 def create_checkout_session():
     try:
-        session = stripe.checkout.Session.create(
-            ui_mode = 'embedded',
+        checkout_session = stripe.checkout.Session.create(
             line_items=[
                 {
                     # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -198,20 +197,34 @@ def create_checkout_session():
                 },
             ],
             mode='payment',
-            return_url=YOUR_DOMAIN + '/return.html?session_id={CHECKOUT_SESSION_ID}',
+            success_url=YOUR_DOMAIN + '/success',
+            cancel_url=YOUR_DOMAIN + '/cancel',
         )
     except Exception as e:
         return str(e)
 
-    return jsonify(clientSecret=session.client_secret)
+    return redirect(checkout_session.url, code=303)
 
 @app.route('/session-status', methods=['GET'])
 def session_status():
   session = stripe.checkout.Session.retrieve(request.args.get('session_id'))
 
   return jsonify(status=session.status, customer_email=session.customer_details.email)
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+@app.route("/cancel")
+def cancel():
+    return render_template("cancel.html")
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
  
 
 
 # Very important to disable debug mode
-app.run(host="0.0.0.0", port=4004, debug=True)
+if __name__ == '__main__':
+    app.run(port=4004, debug=True)
