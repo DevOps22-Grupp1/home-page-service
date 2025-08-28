13b31fba2baa5c3295ab413a118cf8925d0229ee
webcrunch <jarllindquist@gmail.com>
2025-08-28 13:48:26 +0200

    Update delete_cart route to include product ID in the delete request

--- backend/app.py
+++ backend/app.py
@@ -258,8 +258,9 @@ def update_p() -> tuple:
 
 @app.route("/delete_cart/", methods=["GET"])
 def del_cart() -> tuple:
+    p_id = request.args.get("p_id")
     id = request.args.get("id")
-    delete_url = f"http://{order_processing}:{order_port}/api/cart/{id}"
+    delete_url = f"http://{order_processing}:{order_port}/api/cart/{id}/{p_id}"
     response = requests.delete(delete_url)
     if response.status_code == 204:
         # The DELETE request was successful, and there's no response content.
